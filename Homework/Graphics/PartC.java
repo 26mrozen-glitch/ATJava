f1fd963aee478db329a6afe259168f0685d2595f
26mrozen-glitch <26mrozen@derryfield.org>
2025-12-17 12:34:04 -0500

    graphics

---
+++ Homework/Graphics/PartC.java
@@ -0,0 +1,113 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.*;
+
+public class PartC {
+    /*
+     * Change the following method to customize
+     * what is drawn in the JFrame.
+     */
+    public static void draw(Graphics g) {
+        // Set background to white
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 400, 400);
+
+        // Set drawing color for the rose
+        g.setColor(Color.BLACK);
+
+        // Center of the panel
+        int centerX = 200;
+        int centerY = 200;
+
+        // Number of points
+        int steps = 1000;
+
+        double prevX = 0, prevY = 0;
+        boolean firstPoint = true;
+
+        for (int i = 0; i <= steps; i++) {
+            double theta = 2 * Math.PI * i / steps;
+            double r = Math.cos(2 * theta);
+
+        
+            double x = r * Math.cos(theta);
+            double y = r * Math.sin(theta);
+
+            // Scale to fit the 400x400 window
+            int screenX = centerX + (int) (x * 150); // scale factor 150
+            int screenY = centerY - (int) (y * 150); // subtract because screen y-axis is downwards
+
+            if (!firstPoint) {
+                g.drawLine((int) prevX, (int) prevY, screenX, screenY);
+            } else {
+                firstPoint = false;
+            }
+
+            prevX = screenX;
+            prevY = screenY;
+        }
+    }
+
+    public static enum OS {
+        WIN, MAC, LINUX, OTHER
+    }
+
+    public static OS checkOS() {
+        OS val;
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.indexOf("win") >= 0) {
+            val = OS.WIN;
+        } else if (name.indexOf("mac") >= 0) {
+            val = OS.MAC;
+        } else if (name.indexOf("nux") >= 0) {
+            val = OS.LINUX;
+        } else {
+            val = OS.OTHER;
+        }
+        return val;
+    }
+
+    public static void main(String[] args) {
+        final int CONTENT_WIDTH = 400;
+        final int CONTENT_HEIGHT = 400;
+
+        int border_width = 0;
+        int bar_height = 0;
+        OS os = checkOS();
+        switch (os) {
+            case WIN:
+                border_width = 7;
+                bar_height = 30;
+                break;
+            case MAC:
+                border_width = 0;
+                bar_height = 28;
+                break;
+            case LINUX:
+                break;
+            case OTHER:
+                break;
+        }
+
+        int frame_width = CONTENT_WIDTH + 2 * border_width;
+        int frame_height = CONTENT_HEIGHT + bar_height + border_width;
+        JFrame frame = new JFrame();
+        frame.setSize(frame_width, frame_height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setTitle("Four-Leaved Rose");
+
+        JComponent component = new JComponent() {
+            public void paintComponent(Graphics graph) {
+                draw(graph);
+            }
+        };
+
+        frame.add(component);
+        frame.setVisible(true);
+
+        System.out.println("Frame Size   : " + frame.getSize());
+        System.out.println("Frame Insets : " + frame.getInsets());
+        System.out.println("Content Size : " + frame.getContentPane().getSize());
+    }
+}
