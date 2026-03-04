ed2e0469778f89da3d8543b7866921e0b95993b6
26mrozen-glitch <26mrozen@derryfield.org>
2026-03-04 09:37:58 -0500

    code

---
+++ Homework/Lesson 26/PartA.java
@@ -0,0 +1,14 @@
+public class PartA{
+    public static void main(String[] args) {
+
+        // Using a Java provided constant
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+
+        // Using a Java provided static method
+        double squareRoot = Math.sqrt(25);
+
+        System.out.println("Area of circle: " + area);
+        System.out.println("Square root of 25: " + squareRoot);
+    }
+}
